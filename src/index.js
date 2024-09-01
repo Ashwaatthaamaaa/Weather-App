10ef29d492c196f29f1a3bca693fe9f2cbb7e0ce
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-09-01 22:15:14 +0530

    remove fetchGif function

--- src/index.js
+++ src/index.js
@@ -26,20 +26,4 @@ function getCity(){
     getData(city)
 
 }
-
-export async function fetchGif() {
-    try {
-        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=LwQfFbJnHPLkNdANhixn9gQ0xhzQpsaG&s=not-found`, {mode: 'cors'});
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data.data.images.original.url;
-    } catch(error) {
-        console.error('Error:', error);
-        return null; // or a default URL
-    }
-}
-
-
 onSubmit();
